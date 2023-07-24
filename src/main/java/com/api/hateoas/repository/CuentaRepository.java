48bdedf2daea331a29a72430bdbf7c047cb8a667
Gutavo Martinez <gutamvol262@gmail.com>
2023-07-24 16:28:24 -0600

    agregamos un query para ela bono de dinero a la cuenta

--- src/main/java/com/api/hateoas/repository/CuentaRepository.java
+++ src/main/java/com/api/hateoas/repository/CuentaRepository.java
@@ -3,9 +3,16 @@ package com.api.hateoas.repository;
 import com.api.hateoas.model.Cuenta;
 import jakarta.persistence.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
 public interface CuentaRepository  extends JpaRepository <Cuenta,Integer> {
+
+
+    @Query("UPDATE Cuenta c set  c.monto=c.monto + ?1 where  c.id=?2")
+    @Modifying
+    void actualizarMonto(float monto, Integer id);
 }
