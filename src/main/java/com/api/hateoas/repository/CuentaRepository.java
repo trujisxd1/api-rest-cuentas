b9bbc9689baeb83e0018b7f2856c32b7712ffcc7
Gutavo Martinez <gutamvol262@gmail.com>
2023-07-25 10:49:40 -0600

    arreglamos el @query

--- src/main/java/com/api/hateoas/repository/CuentaRepository.java
+++ src/main/java/com/api/hateoas/repository/CuentaRepository.java
@@ -1,7 +1,6 @@
 package com.api.hateoas.repository;
 
 import com.api.hateoas.model.Cuenta;
-import jakarta.persistence.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,5 +13,5 @@ public interface CuentaRepository  extends JpaRepository <Cuenta,Integer> {
 
     @Query("UPDATE Cuenta c set  c.monto=c.monto + ?1 where  c.id=?2")
     @Modifying
-    void actualizarMonto(float monto, Integer id);
+  void  actualizarMonto(float monto, Integer id);
 }
